b62d89440288fba988c03e5841211a77a5a2a677
Bhavish <bhavishpothuraju123@gmail.com>
2022-06-30 23:59:03 +0530

    1st commit

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+
+print("helloWorld");
+
+# getting information from the user....!
+
+country = "india";
+print("i love my " + country);
+
+#datatypes 
+print(type("a"));
+print(type(2));
+print(type(2.5))
+
+# the process of storing a value inside the variable is called 'assignment'.
+#implicit conversion - converting one datatype into another (it happens internally here computer sorts the things.)
+#explicit conversion - converting one datatype (here manually need to be done)
+
+print(7 + 7.8) #IMPLICIT CONVERSION
+print("hello this is " + str(8)) #Explicit Conversion
