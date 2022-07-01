acd6eba6f85a8085967d9428277600f9573f3e7f
Bhavish <bhavishpothuraju123@gmail.com>
2022-07-01 22:17:56 +0530

    4th commit

--- learn_python.py
+++ learn_python.py
@@ -17,3 +17,60 @@ print(type(2.5))
 
 print(7 + 7.8) #IMPLICIT CONVERSION
 print("hello this is " + str(8)) #Explicit Conversion
+
+# Functions :
+ # 'def' keyword is used to define a function. and then function name and colon
+def greeting(name):
+   print ("welcome, " + name)
+
+greeting("bhavish")
+
+def greeting(name, department):
+    print("welcome," + name)
+    print("so you are in "+ department)
+
+greeting("bhavish", "cce")
+
+#return function returns the value from the function
+
+def area_triangle(base, height):
+    return (base*height)/2;
+areaof1sttraingle = area_triangle(2,5)
+areaof2ndttraingle = area_triangle(7,3)
+sum = areaof1sttraingle + areaof2ndttraingle
+
+print("The sum of both areas is: "+ str(sum))
+
+name = "kay"
+number = len(name) * 9
+print(number)
+
+# area of circle
+
+def area_circle(r):
+    pi= 3.14;
+    area = (pi*(r**2))
+    return area;
+
+area1 = area_circle(6)
+print(area1);
+
+### good problem
+# This function compares two numbers and returns them
+# in increasing order.
+def order_numbers(number1, number2):
+	if number2 > number1:
+		return number1, number2
+	else:
+		return number2, number1
+
+# 1) Fill in the blanks so the print statement displays the result
+#    of the function call
+smaller ,bigger = order_numbers(100, 99)
+print(smaller, bigger)
+
+### conditionals
+print(10>1)
+
+## boolean : true or false.
+print("cat"=="cat")
